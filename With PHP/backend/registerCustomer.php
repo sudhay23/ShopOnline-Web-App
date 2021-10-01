d6c9b901cc3b09f08ab5db9226850a243b8ef37a
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-10-01 12:56:25 +0530

    Update registerCustomer.php

--- With PHP/backend/registerCustomer.php
+++ With PHP/backend/registerCustomer.php
@@ -9,15 +9,10 @@ $accesslevel = $_POST['accesslevel'];
 $paymentpreference = $_POST['paymentpreference'];
 $address = $_POST['address'];
 
-    // $host = "localhost";
-    // $dbUsername = "admin";
-    // $dbPassword = "admin";
-    // $dbname = "shoponline";
-
-    $host = "sql206.epizy.com";
-    $dbUsername = "epiz_29762748";
-    $dbPassword = "CrifBpYoOS3lWg";
-    $dbname = "epiz_29762748_shoponline";
+$host = "localhost";
+$dbUsername = "admin";
+$dbPassword = "admin";
+$dbname = "shoponline";
 
     //create connection
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
