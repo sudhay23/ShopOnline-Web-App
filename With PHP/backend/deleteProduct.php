f1b357768144cff055d001c32b8c4dbeba1803ae
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-10-01 12:54:56 +0530

    Update deleteProduct.php

--- With PHP/backend/deleteProduct.php
+++ With PHP/backend/deleteProduct.php
@@ -2,14 +2,10 @@
 
     $productId = $_POST["productID"];
 
-    // $host = "localhost";
-    // $dbUsername = "admin";
-    // $dbPassword = "admin";
-    // $dbname = "shoponline";
-    $host = "sql206.epizy.com";
-    $dbUsername = "epiz_29762748";
-    $dbPassword = "CrifBpYoOS3lWg";
-    $dbname = "epiz_29762748_shoponline";
+$host = "localhost";
+$dbUsername = "admin";
+$dbPassword = "admin";
+$dbname = "shoponline";
 
     //create connection
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
